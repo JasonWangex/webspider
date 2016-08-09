b96b7c3e0994214e7137ac9d78ae83d61d5aa06e
jason <jasonwangex@gmail.com>
2016-08-09 21:27:03 +0800

    存储功能

--- spider/dispacher.py
+++ spider/dispacher.py
@@ -8,21 +8,19 @@ import time
 import threading
 from Queue import Queue
 
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
 shut_down = False
 
-# while not shut_down:
-# def init_spider():
 uid_set = set()
-
 uid_set.add(config.first_uid)
 user_queue = Queue(200)
 content_queue = Queue(200)
 all_uid_list = []
 user_dao.start_session()
-
-
-# content = download.get_content(config.first_url)
-# user = download.get_content(resolver.get_url_by_uid(url_queue.get()))
 
 
 def is_uid_set_full():
@@ -31,6 +29,23 @@ def is_uid_set_full():
 
 def is_uid_set_empty():
     return len(uid_set) == 0
+
+
+def init_spider():
+    global uid_set
+    global content_queue
+    global all_uid_list
+
+    with open('all_uid_list', 'rb') as f_all_uid:
+        all_uid_list = pickle.load(f_all_uid)
+
+    with open('content_queue', 'rb') as f_content_queue:
+        content_list = pickle.load(f_content_queue)
+        for content in content_list:
+            content_queue.put(content, block=True)
+
+    with open('uid_set', 'rb') as f_uid_set:
+        uid_set = pickle.load(f_uid_set)
 
 
 def download_thread():
@@ -46,9 +61,7 @@ def download_thread():
             all_uid_list.append(uid)
             print '开始获取 ', uid
             content = download.get_content(url)
-            while content_queue.full():
-                time.sleep(0.5)
-            content_queue.put(content)
+            content_queue.put(content, block=True)
 
 
 def resolve_thread():
@@ -57,9 +70,7 @@ def resolve_thread():
     global shut_down
     global all_uid_list
     while not shut_down:
-        while content_queue.empty():
-            time.sleep(0.5)
-        content = content_queue.get()
+        content = content_queue.get(block=True)
         try:
             user = resolver.resolve_for_user(content)
             print '开始解析', user.uid
@@ -84,10 +95,12 @@ def followee_url_thread():
             current_user.getFollowees += 1
             uids = resolver.resolve_for_uids(msg)
             for uid in filter(lambda x: x is not None and x not in all_uid_list, uids):
-                while is_uid_set_full():
+                while is_uid_set_full() and not shut_down:
                     time.sleep(0.5)
-                print '将 ', uid, ' 加入对列'
                 uid_set.add(uid)
+            if shut_down:
+                user_dao.save_or_update(current_user)
+                return
         current_user.needGetFollowees = False
         current_user.getFollowees = max_followee_page
         user_dao.save_or_update(current_user)
@@ -97,7 +110,6 @@ def followee_url_thread():
 def follower_url_thread():
     global all_uid_list
     global uid_set
-    current_user = None
     while not shut_down:
         time.sleep(0.5)
         current_user = user_dao.get_user_for_followees()
@@ -108,15 +120,16 @@ def follower_url_thread():
             msg = download.get_followers(hash_id=current_user.hashId, page=current_user.getFollowers)
             current_user.getFollowees += 1
             uids = resolver.resolve_for_uids(msg)
-            for uid in filter(lambda x: lambda x: x is not None and x not in all_uid_list, uids):
-                while is_uid_set_full():
+            for uid in filter(lambda x: x is not None and x not in all_uid_list, uids):
+                while is_uid_set_full() and not shut_down:
                     time.sleep(0.5)
-                print '将 ', uid, ' 加入对列'
                 uid_set.add(uid)
+            if shut_down:
+                user_dao.save_or_update(current_user)
+                return
         current_user.needGetFollowers = False
         current_user.getFollowers = max_follower_page
         user_dao.save_or_update(current_user)
-    user_dao.save_or_update(current_user)
 
 
 def report():
@@ -133,27 +146,42 @@ def report():
 
 
 def listener(thrs):
-    while True:
-        for thr in thrs:
-            if not thr.isAlive():
-                thr.start()
-    time.sleep(5)
+    global shut_down
+    for thr in thrs:
+        thr.start()
+    while raw_input() != 'exit':
+        continue
+    shut_down = True
+    time.sleep(10)
+    for thr in thrs:
+        if thr.isAlive():
+            print thr.name, 'is not shutdown'
+    with open('all_uid_list', 'wb') as f_all_uid:
+        pickle.dump(all_uid_list, file=f_all_uid)
 
+    with open('content_queue', 'wb') as f_content_queue:
+        # queue 无法序列化, 只能转成list
+        content_list = []
+        while not content_queue.empty():
+            content = content_queue.get(block=True)
+            content_list.append(content)
+        pickle.dump(content_list, file=f_content_queue)
 
+    with open('uid_set', 'wb') as f_uid_set:
+        pickle.dump(uid_set, file=f_uid_set)
+
+# spider begin
+init_spider()
 threads = []
 for i in range(1, 5):
     threads.append(threading.Thread(target=download_thread))
 
-t2 = threading.Thread(target=resolve_thread)
-t3 = threading.Thread(target=followee_url_thread)
-t4 = threading.Thread(target=follower_url_thread)
-t6 = threading.Thread(target=report)
+t2 = threading.Thread(target=resolve_thread, name='resolve_thread')
+t3 = threading.Thread(target=followee_url_thread, name='followee_url_thread')
+t4 = threading.Thread(target=follower_url_thread, name='follower_url_thread')
+t6 = threading.Thread(target=report, name='report')
 threads.append(t2)
 threads.append(t3)
 threads.append(t4)
 threads.append(t6)
-# t2.start()
-# t3.start()
-# t4.start()
-# t6.start()
-threading.Thread(target=listener(threads)).start()
+threading.Thread(target=listener, args=(threads,)).start()
